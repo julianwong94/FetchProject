f57e73b8c75261f9657ce15c1f9d11ff8c0c31fe
Julian Wong <julian.wong94@berkeley.edu>
2025-03-17 15:20:58 -0700

    add basenames

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,19 +3,22 @@ import { Login } from "./pages/LoginPage";
 import { DogMatchPage } from "./pages/DogMatchPage";
 import { NavigationLayout } from "./components/NavigationLayout";
 
-export const router = createBrowserRouter([
-  {
-    path: "/login",
-    Component: Login,
-  },
-  {
-    path: "/",
-    element: <NavigationLayout />,
-    children: [
-      {
-        index: true,
-        Component: DogMatchPage,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/login",
+      Component: Login,
+    },
+    {
+      path: "/",
+      element: <NavigationLayout />,
+      children: [
+        {
+          index: true,
+          Component: DogMatchPage,
+        },
+      ],
+    },
+  ],
+  { basename: "/FetchProject/" }
+);
